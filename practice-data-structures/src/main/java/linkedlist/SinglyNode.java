9e4b5475b011ef431198d7251de35f290cde9389
Trung Dang <trungdang@tecace.com>
2019-04-15 18:02:03 -0700

    Added singly linked list implementation

---
+++ practice-data-structures/src/main/java/linkedlist/SinglyNode.java
@@ -0,0 +1,11 @@
+package linkedlist;
+
+public class SinglyNode {
+    int data;
+    SinglyNode next;
+
+    public SinglyNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
